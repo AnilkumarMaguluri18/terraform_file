e8fde4e982676195a604ee2ac57d97ee071ad4ba
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-26 15:24:50 -0500

    Update vpc_aws.tf

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -64,20 +64,3 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-resource "aws_instance" "Anil_ec2_instance" {
-  ami                          = "ami-0cd3c7f72edd5b06d"
-  instance_type                = "t2.micro"
-  key_name                     = "Kumar"
-  vpc_security_group_ids       = [aws_security_group.my_security_group.id]
-  subnet_id                    = aws_subnet.Anil_public_subnet.id
-  associate_public_ip_address  = true
-
-  tags = {
-    Name = "AnilEC2Instance"
-  }
-}
-
-resource "aws_route_table_association" "Anil_subnet_association" {
-  subnet_id      = aws_subnet.Anil_public_subnet.id
-  route_table_id = aws_route_table.Anil_public_route.id
-}

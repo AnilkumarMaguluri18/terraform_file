2797f62138c84ec5127eccea97bec34d3cc1a1a0
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-26 18:09:08 -0500

    Update vpc_aws.tf

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -64,3 +64,30 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
+
+resource "aws_instance" "Anil_ec2_instance" {
+  ami                          = "ami-09694bfab577e90b0"
+  instance_type                = "t2.micro"
+  key_name                     = "JenkinsKey"
+  vpc_security_group_ids       = [aws_security_group.my_security_group.id]
+  subnet_id                    = aws_subnet.Anil_public_subnet.id
+  associate_public_ip_address  = true
+
+  tags = {
+    Name = "AnilEC2Instance"
+  }
+}
+
+resource "aws_route_table_association" "Anil_subnet_association" {
+  subnet_id      = aws_subnet.Anil_public_subnet.id
+  route_table_id = aws_route_table.Anil_public_route.id
+
+  depends_on = [
+    aws_vpc.Anil_vpc,
+    aws_subnet.Anil_public_subnet,
+    aws_security_group.my_security_group,
+  ]
+}
+
+
+
